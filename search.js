0bcb2986260c92e40582de8feb531220c5086a6d
Mitchell Devlin <mitchell.r.devlin@gmail.com>
2018-04-11 06:50:20 -0400

    searching by filters

--- search.js
+++ search.js
@@ -1,16 +1,36 @@
+
 function searchState() {
-    var val = document.getElementById("state");
-    var md = document.getElementById("DEc");
-    var de = document.getElementById("MDc");
-    var b = document.getElementById("b");
-    b.innerHTML = "<p>Test</p>";
+    var stateSel = document.getElementById("stateSelect");
+    var val = document.getElementById("state").value;
+    var md = document.getElementById("MDc");
+    var de = document.getElementById("DEc");
+    var mdCity = document.getElementById("cityMD");
+    var deCity = document.getElementById("cityDE");
+    var submitb = document.getElementById("submitbutton");
+    var finalsearch = document.getElementById("finalsearch");
+
     if(val == "MD") {
         de.style.display = "none";
-        md.style.display = "inline";
+        md.style.display = "inline-block";
+        deCity.style.display = "none";
+        mdCity.style.display = "inline-block";
+        stateSel.innerHTML = "<h1>State: Maryland<h1>";
+        md.insertAdjacentHTML("beforeend", "<br><br>");
     }
     else if (val == "DE") {
-        val.value = "DE";
-        de.style.display = "inline";
+        de.style.display = "inline-block";
         md.style.display = "none";   
+        deCity.style.display = "inline-block";
+        mdCity.style.display = "none";
+        stateSel.innerHTML = "<h1>State: Delaware<h1>";
+        de.insertAdjacentHTML("beforeend", "<br><br>");
     }
-};
+    submitb.style.display = "none";
+    finalsearch.style.display = "inline-block";
+    return false;
+}
+
+function searchMap() {
+    window.location = "http://localhost:8000/map.html";
+    return false;
+}
