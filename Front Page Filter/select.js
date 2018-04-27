2099476c59de9d0112958ee750976b7cb569b533
olean123 <31897877+olean123@users.noreply.github.com>
2018-04-27 17:35:59 -0400

    Add files via upload

--- Front Page Filter/select.js
+++ Front Page Filter/select.js
@@ -70,3 +70,14 @@ window.onload = function () {
 		}
 	}	
 }
+
+function SearchMap(){
+	var stateVal = document.getElementById("stateSel").value;	
+	var cityVal = document.getElementById("citySel").value;
+	var countyVal = document.getElementById("countySel").value;
+
+	if( (stateVal != null) && (cityVal != null) && (countVal != null) ){
+		window.location = "http://localhost:8000/map.html";
+	}
+	return false;
+}
